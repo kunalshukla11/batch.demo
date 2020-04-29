d30d09bdfcd9fef96a1563b2d3f14aedc4ce4d2c
narendra <narendra1192cs@gmail.com>
2020-04-29 12:18:29 +0100

    adding space

--- src/test/java/com/spring/batch/example/batch/demo/ApplicationTests.java
+++ src/test/java/com/spring/batch/example/batch/demo/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spring.batch.example.batch.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,7 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
