9607232af2afcf0dff2d91d6acb29c8cd19f9c42
narendra <narendra1192cs@gmail.com>
2020-04-29 15:03:09 +0100

    Adding a dummy test

---
+++ src/test/java/com/spring/batch/example/batch/demo/MainTestFile.java
@@ -0,0 +1,18 @@
+package com.spring.batch.example.batch.demo;
+
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTestFile {
+
+    @Test
+    public void DummyTest(){
+        assertEquals(1, 1);
+        System.out.println("Test completed See i have now been completed");
+
+    }
+}
